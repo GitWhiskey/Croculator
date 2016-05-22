13e5ea96a3fe2cdf80c126e0331e54a2ad3a1d36
Max Khabart <max.khabrat@gmail.com>
2016-05-22 14:30:16 +0300

    Добавлены комментарии в SimpleFraction, и проверка на деление на ноль при перевороте второй дроби, в операции деления.

--- src/main/java/SimpleFraction.java
+++ src/main/java/SimpleFraction.java
@@ -1,13 +1,23 @@
+/**
+ * Класс "простая дробь". Реализует базовые операции над простыми дробями, такие как сложение, вычитание, умножение и
+ * деление.
+ */
 public class SimpleFraction {
 
-    private int numerator;
-    private int denominator;
+    private int numerator; //Числитель
+    private int denominator;  //Знаменатель
 
     public SimpleFraction(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
     }
 
+    /**
+     * Выполняет сложение двух простых дробей.
+     * @param a - слагаемое 1
+     * @param b - слагаемое 2
+     * @return - результат сложения двух простых дробей a+b.
+     */
     public static SimpleFraction add(SimpleFraction a, SimpleFraction b) {
         SimpleFraction result;
         if(a.denominator == b.denominator) {
@@ -20,6 +30,12 @@ public class SimpleFraction {
         }
     }
 
+    /**
+     * Выполняет операцию вычитания двух простых дробей.
+     * @param a - уменьшаемое
+     * @param b - вычитаемое
+     * @return - разность a-b.
+     */
     public static SimpleFraction subtract(SimpleFraction a, SimpleFraction b) {
         SimpleFraction result;
         if(a.denominator == b.denominator) {
@@ -32,11 +48,27 @@ public class SimpleFraction {
         }
     }
 
+    /**
+     * Выполняет операцию умножени двух простых дробей.
+     * @param a - множитель 1
+     * @param b - множитель 2
+     * @return - результам умножения a*b.
+     */
     public static SimpleFraction multiply(SimpleFraction a, SimpleFraction b) {
         return new SimpleFraction(a.numerator*b.numerator, a.denominator*b.denominator);
     }
 
-    public static SimpleFraction divide(SimpleFraction a, SimpleFraction b) {
+    /**
+     * Выполняет операцию деления простодроби a на простую дробь b.
+     * @param a - делимое
+     * @param b - делитель
+     * @return - результат деления a/b.
+     * @throws ArithmeticException - исключение выбрасывается делитель b сожержит ноль в числителе.
+     */
+    public static SimpleFraction divide(SimpleFraction a, SimpleFraction b) throws ArithmeticException {
+        if(b.numerator == 0) {
+            throw new ArithmeticException("Недопустимая операция. Деление на ноль.");
+        }
         return new SimpleFraction(a.numerator*b.denominator, a.denominator*b.numerator);
     }
 
